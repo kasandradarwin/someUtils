a0fd5c78ec2fa58ca3e02dcd329f29c2b8629473
Kasandra Darwin <kasandradarwin@gmail.com>
2022-09-15 21:28:24 +0000

    fixed spacing added power function

--- utils.js
+++ utils.js
@@ -9,7 +9,13 @@ product(5,5)
 // divides two numbers
 
 function divide(num1,num2){
-console.log(num1/num2)
+    console.log(num1/num2)
 
+}
+
+// to the power of
+
+function power(num1,num2){
+    console.log(num1 ** num2)
 }
 
