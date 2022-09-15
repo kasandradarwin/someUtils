7119527e4c4b08252eed4743223d3802735b4140
Kasandra Darwin <kasandradarwin@gmail.com>
2022-09-15 21:33:30 +0000

    added subtract function, function calls

--- utils.js
+++ utils.js
@@ -12,10 +12,20 @@ function divide(num1,num2){
     console.log(num1/num2)
 
 }
+divide(5,5)
 
 // to the power of
 
 function power(num1,num2){
     console.log(num1 ** num2)
 }
+
+power(5,5)
+//subtract two
+
+function subtract(num1, num2){
+    console.log(num1-num2)
+}
+
+subtract(5,5)
 
